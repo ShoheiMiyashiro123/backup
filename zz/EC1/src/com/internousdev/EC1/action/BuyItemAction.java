4e5eb39d2c7fe317100333206888c0ae93a15ffa
ShoheiMiyashiro123 <velonica0301@gmail.com>
2018-01-17 22:19:53 +0900

    222

---
+++ zz/EC1/src/com/internousdev/EC1/action/BuyItemAction.java
@@ -0,0 +1,11 @@
+package com.internousdev.EC1.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BuyItemAction extends ActionSupport{
+
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
