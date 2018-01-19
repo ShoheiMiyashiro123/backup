d0bae21d7df61b98bb71a823179621d31afc4a06
ShoheiMiyashiro123 <velonica0301@gmail.com>
2018-01-19 22:16:29 +0900

    20180119_2

---
+++ 20180119_2/EC1/src/com/internousdev/EC1/action/MyPageAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.EC1.action;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MyPageAction extends ActionSupport implements SessionAware {
+
+}
---
+++ 20180119_2/EC1/src/com/internousdev/EC1/util/DBConnector.java
@@ -0,0 +1,30 @@
+package com.internousdev.EC1.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	//jdbcドライバのクラス名のことである。
+	public static String driverName = "com.mysql.jdbc.Driver";
+	//ユーザ名をrootにする。
+	public static String userName = "root";
+	//jdbc:mysql://(サーバ名)/(データベース名)の形をとる。
+	public static String url = "jdbc:mysql://localhost/ecsite";
+	//入力用パスワードの用意
+	public static String password = "mysql";
+
+	public Connection getConnection(){
+		Connection con = null;
+		try{
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url,userName,password);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+}
